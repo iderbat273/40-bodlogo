9c8cb17a70e3759d72c408fe555dbf0c7fbc164e
unknown <idreexp@gmail.com>
2025-06-10 16:53:42 +0800

    Dasgal 1

--- Dasgal1/Solution.java
+++ Dasgal1/Solution.java
@@ -1,44 +1,20 @@
-import java.io.*;
-import java.util.*;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Solution {
 
+    public static void extraLongFactorials(int n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int Q = scanner.nextInt();  
-        scanner.nextLine(); 
-        StringBuilder S = new StringBuilder(); 
-        Stack<String> history = new Stack<>(); 
-        for (int i = 0; i < Q; i++) {
-            String[] operation = scanner.nextLine().split(" ");
-
-            switch (operation[0]) {
-                case "1":  // append
-                    history.push(S.toString());  
-                    S.append(operation[1]);      
-                    break;
-
-                case "2":  // delete
-                    int k = Integer.parseInt(operation[1]);
-                    history.push(S.toString());  
-                    S.delete(S.length() - k, S.length()); 
-                    break;
-
-                case "3":  // print
-                    int pos = Integer.parseInt(operation[1]);
-                    if (pos > 0 && pos <= S.length()) {
-                        System.out.println(S.charAt(pos - 1));
-                    }
-                    break;
-
-                case "4":  // undo
-                    if (!history.isEmpty()) {
-                        S = new StringBuilder(history.pop()); 
-                    }
-                    break;
-            }
-        }
-
+        int n = scanner.nextInt();
+        extraLongFactorials(n);
         scanner.close();
     }
 }
